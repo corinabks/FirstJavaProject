2618fe6cf0f247ef6a50f1f552a66006b249c0b3
corinabks <corinabks@gmail.com>
2019-02-07 18:50:23 +0200

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,32 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int x = 7;
+        int y = 8;
+        int z = x + y;
+        int m = 20;
+        int n = 5;
+        int o = m / n;
+        System.out.println("1.Hello");
+        System.out.println("Corina");
+        System.out.println("2.The addition of x and y is:" + z);
+        System.out.println("3.The result of the division is:" + o);
+
+        int a = (-5 + 8 * 6);
+        System.out.println("4.The result of a =" + a);
+
+        int b = (55 + 9) % 9;
+        System.out.println("The result of b =" + b);
+
+        int c = 20 + (-3 * 5) / 8;
+        System.out.println("The result of c =" + c);
+
+        int d = 5 + (15 / 3 * 2) - (8 % 3);
+        System.out.println("The result of d =" +d);
+
     }
+
+
 }
+
+
